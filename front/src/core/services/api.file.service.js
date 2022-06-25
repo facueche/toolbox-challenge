2d4d20378b7a83b135e63c9c991eba85a74562ce
Facu Echenique <facueche@gmail.com>
2022-06-25 02:24:57 -0300

    chore: remove no-cors request

--- front/src/core/services/api.file.service.js
+++ front/src/core/services/api.file.service.js
@@ -1,12 +1,10 @@
 export const getFiles = async () => {
     try {
         const response = await fetch("http://localhost:3000/files/data", {
-            mode: "no-cors",
             headers: {
                 Accept: "application/json",
             }
         });
-        console.log(response);
         const jsonResponse = await response.json();
 
         if (response.status !== 200)
