0b7c3e1c6416c66a1ea6e3084bd397065f2ad4c0
Facu Echenique <facueche@gmail.com>
2022-06-25 02:24:03 -0300

    feat: allow all request with cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const http = require("http");
 const filesRoutes = require("./routes/files");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 app.use("/", filesRoutes);
 
